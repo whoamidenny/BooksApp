51fcdba7be3a25c8a44a915f27e695b9f1da59d6
Артур Дученко <arthur@fulcrum.rocks>
2020-07-22 21:31:54 +0300

    Add SignUp screen

--- src/screens/Auth/SignUp/index.js
+++ src/screens/Auth/SignUp/index.js
@@ -2,12 +2,46 @@ import React from 'react';
 
 import {View, Text} from 'react-native';
 import {DefaultButton} from '../../../components/Buttons';
+import {MainBlock} from '../../../components/Blocks';
+
+import SignUpIcon from '../../../assets/images/signup.svg';
+import {scaledSize} from '../../../styles';
+
+import styles from './styles';
+import globalStyles from '../../../styles/globalStyles';
+import {DefaultInput} from '../../../components/Inputs';
 
 function SignUp() {
   return (
-    <View>
-      <DefaultButton title="Register" />
-    </View>
+    <MainBlock>
+      <View style={styles.mainBlock}>
+        <Text style={globalStyles.headerText}>Sign Up</Text>
+        <View>
+          <DefaultInput placeholder="First & Last Name" />
+          <DefaultInput placeholder="Email" />
+          <DefaultInput placeholder="Password" />
+
+          <DefaultButton
+            title="Register"
+            containerStyle={{marginVertical: scaledSize(55)}}
+          />
+        </View>
+        <Text style={[styles.regularText, {textAlign: 'center'}]}>or</Text>
+        <View style={styles.row}>
+          <View style={styles.fbButton}>
+            <Text style={[styles.socialLabel, {color: 'white'}]}>
+              Continue with
+            </Text>
+          </View>
+          <View style={styles.googleButton}>
+            <Text style={styles.socialLabel}>Continue with</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.bottomContainer}>
+        <SignUpIcon height={scaledSize(659)} width={scaledSize(913)} />
+      </View>
+    </MainBlock>
   );
 }
 
--- src/screens/Auth/SignUp/styles.js
+++ src/screens/Auth/SignUp/styles.js
@@ -1,3 +1,53 @@
-import {StyleSheet} from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import {scaledSize} from '../../../styles';
+import fonts from '../../../constants/fonts';
 
-export default StyleSheet.create({});
+export default EStyleSheet.create({
+  bottomContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: scaledSize(20),
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  forgetText: {
+    fontFamily: fonts.circeRegular,
+    fontSize: scaledSize(36),
+    color: '$text',
+  },
+  fbButton: {
+    height: scaledSize(160),
+    borderRadius: scaledSize(80),
+    flex: 1,
+    marginRight: 10,
+    backgroundColor: '#1877f2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  googleButton: {
+    height: scaledSize(160),
+    borderRadius: scaledSize(80),
+    flex: 1,
+    marginLeft: 10,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  socialLabel: {
+    fontFamily: fonts.circeRegular,
+    fontSize: scaledSize(40),
+    color: '#0b1e35',
+  },
+  mainBlock: {
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  regularText: {
+    fontFamily: fonts.circeRegular,
+    fontSize: scaledSize(36),
+    color: '$text',
+  },
+});
