c19a139f24e44cfdeb4005a61cf3cbcac23c2bc9
Arthur Duchenko <arthur@fulcrum.rocks>
2020-10-15 20:07:08 +0300

    Add support redux in devtools

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
 import logger from 'redux-logger';
 import {persistStore} from 'redux-persist';
 
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunkMiddleware, logger)));
 const persistor = persistStore(store);
 
 export {store, persistor};
